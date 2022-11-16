efc5f606595688d7d263d456acf46eecb6d052d0
Thomas Cowie <thomas.cowie12@gmail.com>
2022-11-15 22:38:13 -0500

    First Read

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.awt.*;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            File file = new File("Test/pom.xml");
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document doc = docBuilder.parse(file);
+        } catch(Exception e) {
+            e.printStackTrace();
+
+        }
+
     }
 }
